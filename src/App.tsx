94723574ec185f4b00995ebc17eddff2b04f1353
7wataaa <nn.contact.n@gmail.com>
2023-04-26 04:45:01 +0900

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import {
   Box,
+  Button,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightAddon,
+  InputRightElement,
   Table,
   TableCaption,
   TableContainer,
@@ -13,11 +18,31 @@ import {
 } from '@chakra-ui/react';
 import type { InputProps } from '@chakra-ui/react';
 import './App.css';
+import { useState } from 'react';
+import { CopyIcon } from '@chakra-ui/icons';
+
+type FixedLengthArray<T, N extends number, A extends any[] = []> = A extends {
+  length: N;
+}
+  ? A
+  : FixedLengthArray<T, N, [...A, T]>;
+
+type ClassSchedule = FixedLengthArray<string, 5>;
+
+type FormStrs = FixedLengthArray<ClassSchedule, 5>;
 
 function App() {
+  const [formStrs, setFormStrs] = useState<FormStrs>([
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+    ['', '', '', '', ''],
+  ]);
+
   return (
-    <Box>
-      <TableContainer p={['2vw', '10vw']}>
+    <Box p={['0 2vw', '0 10vw']}>
+      <TableContainer>
         <Table variant="simple">
           <TableCaption>授業コードを入力してください</TableCaption>
           <Thead>
@@ -30,105 +55,59 @@ function App() {
             </Tr>
           </Thead>
           <Tbody>
-            <Tr>
-              <Td>
-                <CodeInput placeholder="ex) B0B002A0" autoFocus />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-            </Tr>
-
-            <Tr>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-            </Tr>
-
-            <Tr>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-            </Tr>
-
-            <Tr>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-            </Tr>
-
-            <Tr>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-              <Td>
-                <CodeInput />
-              </Td>
-            </Tr>
+            {formStrs.map((row, i) => (
+              <Tr key={i}>
+                {row.map((str, j) => (
+                  <Td key={j}>
+                    <CodeInput
+                      value={str}
+                      onChange={(event) =>
+                        setFormStrs(
+                          (prev) =>
+                            prev.map((r, k) =>
+                              i === k
+                                ? [
+                                    ...r.slice(0, j),
+                                    event.target.value,
+                                    ...r.slice(j + 1),
+                                  ]
+                                : r
+                            ) as FormStrs
+                        )
+                      }
+                    />
+                  </Td>
+                ))}
+              </Tr>
+            ))}
           </Tbody>
-          <Tfoot></Tfoot>
         </Table>
       </TableContainer>
+
+      <ResultOutput />
     </Box>
   );
 }
 
 function CodeInput({ ...props }: InputProps) {
   return <Input variant="flushed" {...props} />;
+}
+
+function ResultOutput() {
+  return (
+    <InputGroup p={['2vw', '10vw']} size="lg">
+      <Input disabled value={'https://example.com'} />
+      <InputRightAddon>
+        <Button
+          aria-label="copy result url"
+          colorScheme="teal"
+          leftIcon={<CopyIcon>Copy</CopyIcon>}
+        >
+          Copy
+        </Button>
+      </InputRightAddon>
+    </InputGroup>
+  );
 }
 
 export default App;
