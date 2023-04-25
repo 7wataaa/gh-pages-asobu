746c47ced41d834ac15f62242304b35d526ad928
7wataaa <65275342+7wataaa@users.noreply.github.com>
2023-04-25 12:54:59 +0000

    itiji

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,134 @@
-import React from 'react';
-import logo from './logo.svg';
+import {
+  Box,
+  Input,
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Tfoot,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/react';
+import type { InputProps } from '@chakra-ui/react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <TableContainer p={['2vw', '10vw']}>
+        <Table variant="simple">
+          <TableCaption>授業コードを入力してください</TableCaption>
+          <Thead>
+            <Tr>
+              <Th>月曜</Th>
+              <Th>火曜</Th>
+              <Th>水曜</Th>
+              <Th>木曜</Th>
+              <Th>金曜</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>
+                <CodeInput placeholder="ex) B0B002A0" autoFocus />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+            </Tr>
+
+            <Tr>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+              <Td>
+                <CodeInput />
+              </Td>
+            </Tr>
+          </Tbody>
+          <Tfoot></Tfoot>
+        </Table>
+      </TableContainer>
+    </Box>
   );
+}
+
+function CodeInput({ ...props }: InputProps) {
+  return <Input variant="flushed" {...props} />;
 }
 
 export default App;
